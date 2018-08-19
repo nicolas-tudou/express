754702996f2065e85209bcde37f9848b5445622d
1 <13261205116@163.com>
2018-08-19 23:19:17 +0800

    commit for mysql

--- express_cookie.js
+++ express_cookie.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const mysql = require('mysql');
 const app = express();
+
+var pool = mysql.createPool({
+    host: 'localhost',  
+    user: 'root',  
+    password: 'root',  
+    database: 'test',  
+    port: 3306
+})
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
@@ -9,8 +20,23 @@ app.get('/', (req, res) => {
     console.log('server start')
 })
 
-app.post('/login', (req, res) => {
-    
+app.post('/login', urlencodedParser, (req, res) => {
+    var sql = `select id from person_info where name=? and password=?`
+    pool.getConnection((err, connect) => {
+        if(err) {
+            throw err;
+        }else {
+            connect.query(sql, [req.body.username, req.body.password], (err, values, fileds) => {
+                if(values.length > 0) {
+                    res.cookie('userId', values[0].id, {maxAge: 100});
+                    res.send('登陆成功');
+                }else {
+                    res.send('密码错误');
+                }
+                connect.release();
+            })
+        }
+    })
 })
 
 
